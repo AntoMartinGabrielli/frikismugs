03b5766b90bb0b0c49bd504d45071ded80d3ef5a
Anto.Martin <almartingab@gmail.com>
2023-05-10 02:02:14 -0300

    Intento de detalle del producto

--- src/Components/ItemList/ItemList.jsx
+++ src/Components/ItemList/ItemList.jsx
@@ -5,7 +5,7 @@ const ItemList = ({listaTazas}) =>{
     return (
         <div className='itemList'>
             {listaTazas.map(tazas => 
-                <Item key={tazas.id} img={tazas.img} stock={tazas.stock} description={tazas.descripcion} price={tazas.precio} title={tazas.titulo} />)}
+                <Item key={tazas.id} img={tazas.img} stock={tazas.stock} descripcion={tazas.descripcion} precio={tazas.precio} titulo={tazas.titulo} />)}
         </div>
     )
 }
